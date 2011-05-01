d1b5643435ef1e910fef2de5a234fa234e98c115
Matt Phillips <mattp@cpan.org>
2011-05-01 16:49:01 -0400

    Added: validation on routes passin

--- lib/WWW/GitHub/PostReceiveHook.pm
+++ lib/WWW/GitHub/PostReceiveHook.pm
@@ -2,16 +2,25 @@ use Web::Simple 'WWW::GitHub::PostReceiveHook';
 
 {
     package WWW::GitHub::PostReceiveHook;
+
+    use Sub::Quote;
     use JSON;
-    use Data::Dump;
 
     has routes => (
         is       => 'rw',
-        required => 1
-    );
+        required => 1,
+        isa      => sub {
+            # must be hash
+            die 'Routes must be a HASH ref.' unless ref $_[0] eq 'HASH';
 
-    sub BUILD {
-    }
+            while (my ($key, $value) = each %{ $_[0] }) {
+                # must match simple path
+                die 'Routes must be of the form qr{^/\w+/?}' if $key !~ m{^/\w+/?$};
+                # must map to a coderef
+                die 'route must map to CODE ref.' unless ref $value eq 'CODE';
+            }
+        },
+    );
 
     sub dispatch_request {
 
@@ -21,6 +30,7 @@ use Web::Simple 'WWW::GitHub::PostReceiveHook';
 
             sub (%payload=) {
                 my ( $self, $p ) = @_;
+                use Data::Dump;
                 dd from_json $p;
                 [ 200, [ 'Content-type' => 'text/plain' ], ['OK'] ];
               }
