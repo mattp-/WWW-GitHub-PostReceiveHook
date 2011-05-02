0384aa96dce458ced64283d7a86626d1a7c0c326
Matt Phillips <mattp@cpan.org>
2011-05-01 21:14:27 -0400

    Small tweaks

--- lib/WWW/GitHub/PostReceiveHook.pm
+++ lib/WWW/GitHub/PostReceiveHook.pm
@@ -1,18 +1,19 @@
 use Web::Simple 'WWW::GitHub::PostReceiveHook';
-
 {
     package WWW::GitHub::PostReceiveHook;
 
-    use Sub::Quote;
-    use JSON;
+    use Try::Tiny;
+    use JSON::Any;
 
     has routes => (
-        is       => 'rw',
-        required => 1,
-        isa      => sub {
+        is        => 'rw',
+        predicate => 'has_routes',
+        required  => 1,
+        isa       => sub {
             # must be hash
             die 'Routes must be a HASH ref.' unless ref $_[0] eq 'HASH';
 
+            # validate each route
             while (my ($key, $value) = each %{ $_[0] }) {
                 # must match simple path
                 die 'Routes must be of the form qr{^/\w+/?}' if $key !~ m{^/\w+/?$};
@@ -26,18 +27,33 @@ use Web::Simple 'WWW::GitHub::PostReceiveHook';
 
         sub (POST + /*) {
             my ( $self, $path ) = @_;
-            return if !$self->has_routes;
 
+            # only pass along the request if it matches a given path
+            return if ! $self->has_routes || ! $self->routes->{ "/$path" };
+
+            # catch the payload
             sub (%payload=) {
-                my ( $self, $p ) = @_;
-                use Data::Dump;
-                dd from_json $p;
+                my ( $self, $payload ) = @_;
+
+                try {
+                    # deserialize
+                    my $json = JSON::Any->jsonToObj( $payload );
+
+                    # callback
+                    $self->routes->{ "/$path" }->( $json );
+                }
+                catch {
+                    warn "Exception: /$path: attempted to trigger callback but failed:\n$_";
+
+                    # override the default 200 OK
+                    return [ 400, [ 'Content-type' => 'text/plain' ], ['Bad Request'] ];
+                };
+
                 [ 200, [ 'Content-type' => 'text/plain' ], ['OK'] ];
-              }
-          }, sub () {
-            [ 405, [ 'Content-type', 'text/plain' ], ['Method not allowed'] ];
-          }
+            }
+        },
     }
+
 }
 
 1;
